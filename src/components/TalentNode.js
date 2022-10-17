f555d241018f1b0392625d72eff900604d4f0574
TheConcepteur <damien.lefebvre.mail@gmail.com>
2022-10-17 15:20:47 +0200

    :sparkles: Fix link between talent in build

--- src/components/TalentNode.js
+++ src/components/TalentNode.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { Tooltip } from 'flowbite-react';
 import HyperLinkTooltip from './HyperLinkTooltip';
 import { AppContext } from '../context/AppContext';
 
 function TalentNode(props) {
 	const column = props.column;
+	const id = props.id;
+	const currentDiv = useRef(null);
 	const y = props.y;
 	const x = props.x;
 	const type = props.type;
@@ -32,9 +34,8 @@ function TalentNode(props) {
 	return (
 		<React.Fragment key={'column' + y}>
 			<div
-				className={`separator min-w-[20px] min-h-[1px] w-[54px] h-[1px] column${y} ${
-					column !== undefined && column.before_id !== '' ? 'border -mt-1' : ''
-				} ${y === 1 ? 'hidden' : ''}`}
+				ref={currentDiv}
+				className={`flex max-h-[74px] separator min-w-[20px] min-h-[1px] w-[54px] column${y} ${y === 1 ? 'hidden' : ''}`}
 			></div>
 			<div className={`flex flex-col justify-between min-w-[54px] ${x - 1 === 0 ? 'place-self-start items-center' : ''}`}>
 				{x - 1 === 0 ? <div className="mb-2 font-bold bg-white text-black rounded-md px-1">{(y - 1) * 3}</div> : null}
@@ -58,13 +59,15 @@ function TalentNode(props) {
 							onContextMenu={(e) => removePoint(e, column, type)}
 							onClick={() => addPoint(column, type)}
 						>
-							<div className="rounded-full" style={{ boxShadow: isHightlight ? '0px 0px 2px 3px red' : '' }}>
+							<div id={id} className="rounded-full" style={{ boxShadow: isHightlight ? '0px 0px 2px 3px red' : '' }}>
 								<img
 									loading="lazy"
 									className={`${
 										profPoint[column.position] === undefined || profPoint[column.position] === 0 ? 'contrast-0' : ''
 									} rounded-full border-4 w-[54px] `}
 									src={`img/icons/${column.position === '0|0' ? 'CoreTalentIcon' : 'TalentIcon'}/${column.icon}.png`}
+									width={54}
+									height={54}
 									alt="Icon"
 								/>
 							</div>
