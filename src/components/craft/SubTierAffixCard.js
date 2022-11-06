59ecd7b71ba5f31ce2ecfad3ec54eed90790fa83
JT Maupin <jmsMaupinRC@gmail.com>
2022-11-06 02:57:18 -0500

    Fixed introduced bug where some mods were not properly blocked

--- src/components/craft/SubTierAffixCard.js
+++ src/components/craft/SubTierAffixCard.js
@@ -38,23 +38,27 @@ const SubTierAffiixCard = ({ mod, ember }, ref) => {
 	}
 
 	const onClick = (tier) => {
-		if (ember?.modifier_type === MODIFIER_TYPE.PRE_FIX) {
-            if (craftedItem.prefix.length >= 3) {
-                toast.error('ERROR: PREFIXES FULL');
-                return;
-            }
+		if (!isExcluded(tier)) {
+			if (ember?.modifier_type === MODIFIER_TYPE.PRE_FIX) {
+				if (craftedItem.prefix.length >= 3) {
+					toast.error('ERROR: PREFIXES FULL');
+					return;
+				}
 
-            setCraftedItem({...craftedItem, prefix: [...craftedItem.prefix, tier]})
-        } else if (ember?.modifier_type === MODIFIER_TYPE.POST_FIX){
-            if (craftedItem.postfix.length >= 3) {
-                toast.error('ERROR: POSTFIXES FULL');
-                return;
-            }
+				setCraftedItem({...craftedItem, prefix: [...craftedItem.prefix, tier]})
+			} else if (ember?.modifier_type === MODIFIER_TYPE.POST_FIX){
+				if (craftedItem.postfix.length >= 3) {
+					toast.error('ERROR: POSTFIXES FULL');
+					return;
+				}
 
-            setCraftedItem({...craftedItem, postfix: [...craftedItem.postfix, tier]})
-        } else {
-            console.log("modifer_type error", tier)
-        }
+				setCraftedItem({...craftedItem, postfix: [...craftedItem.postfix, tier]})
+			} else {
+				console.log("modifer_type error", tier)
+			}
+		} else {
+			toast.error('ERROR: SORRY MOD IS BLOCKED')
+		}
 	}
 
 	// for mod with only 1 tier
@@ -68,14 +72,13 @@ const SubTierAffiixCard = ({ mod, ember }, ref) => {
         <>
             {mod?.tiers?.map((tier, key) => {
                 const TEXT_COLOR = highlight === key ? 'text-[#000]' : 'text-[#fff]',
-                      BG_COLOR = highlight === key ? 'bg-[#FFF9E0]' : 'bg-[#333333] hover:bg-[#AAA]',
-                      EXCLUDED = isExcluded(tier);
+                      BG_COLOR = highlight === key ? 'bg-[#FFF9E0]' : 'bg-[#333333] hover:bg-[#AAA]';
 
                 return (
                     <tr
 						key={key}
-						onClick={() => !EXCLUDED && onClick(tier, key)}
-						className={`text-right w-full p-1 cursor-pointer ${BG_COLOR} ${EXCLUDED && 'line-through'}`}
+						onClick={() => onClick(tier, key)}
+						className={`text-right w-full p-1 cursor-pointer ${BG_COLOR} ${isExcluded(tier) && 'line-through'}`}
 					>
 						<td className="text-left gap-2 m-1 pl-2">
 							<HyperLinkTooltip str={tier?.affix} className={TEXT_COLOR} />
